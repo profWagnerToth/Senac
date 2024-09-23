a7ec45833a2bfc7a1feb6832335120ef2fb2102c
Prof Wagner Ramos Toth - Senac Registro <wagner.rtoth@sp.senac.br>
2024-09-23 11:07:10 -0300

    refacture.Wagner app.php

--- MVC-app/Public/core/app.php
+++ MVC-app/Public/core/app.php
@@ -1,43 +1,52 @@
 <?php
-class App{
-    //Definir controlador e método padrão
-    protected $controller = 'UserControler';
+class App {
+    // Definir controlador e método padrão
+    protected $controller = 'UserController'; // Corrigi para 'UserController'
     protected $method = 'index';
     protected $params = [];
 
-    public function __construct(){
-        //Obtem a URL e a transforma em um array
-
+    public function __construct() {
+        // Obtém a URL e a transforma em um array
         $url = $this->parseUrl();
 
-        //Verifica se o controlador existe no diretório de Controladores
-
-        if(file_exists('../app/Controllers/'.$url[0].'.php')){
-            //Remove o primeiro elemento do array (controlador)para continuar o processando
-            unset($user[0]);
-        }
-        //Requerer o arquivo controlador
-        require_once '../app/Controllers/'.$this->controller.'.php';
-        #INSTANCIANDO O CONTROLLER
-        $this->controller = new $this->controller;
-
-        //Verifica se o controlador existe no ditório de Controladores
-
-        if(file_exists('../app/Controllers/'.$url[0].'.php')){
-            //Define o controlador a partir da URL
-            $this->controller=$url[0];
-            //Remove o primeiro elemento do ARRAY (controlador)para continuar processando
+        // Verifica se o controlador existe no diretório de Controladores
+        if (isset($url[0]) && file_exists('../app/Controllers/' . $url[0] . '.php')) {
+            // Define o controlador a partir da URL
+            $this->controller = $url[0];
+            // Remove o primeiro elemento do array (controlador) para continuar o processamento
             unset($url[0]);
         }
 
-        # Requer o arquivo do controlador
-        require_once '../app/controllers/'.$this->controller.'.php';
-        #Instancia o controlador
-        $this->controller = new $this_controller;
+        // Requer o arquivo do controlador
+        require_once '../app/Controllers/' . $this->controller . '.php';
 
-        #Verifica se existe um método (ação) no controlado a ser chamado
-        
+        // Instancia o controlador
+        $this->controller = new $this->controller;
+
+        // Verifica se existe um método (ação) no controlador a ser chamado
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
+                // Define o método (ação) a ser chamado
+                $this->method = $url[1];
+                // Remove o segundo elemento do array (método)
+                unset($url[1]);
+            }
+        }
+
+        // Define os parâmetros restantes como parâmetros para o método
+        $this->params = $url ? array_values($url) : [];
+
+        // Chama o método do controlador com os parâmetros
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
+    // Função para quebrar a URL em partes (Controlador, Método e Parâmetro)
+    public function parseUrl() {
+        if (isset($_GET['url'])) {
+            // Remove barras extras e sanitiza a URL
+            return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
+        return [];
+    }
 }
 ?>
