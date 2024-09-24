fee60095562c2999be914997fc2dbf16e32c0f1c
Prof Wagner Ramos Toth - Senac Registro <wagner.rtoth@sp.senac.br>
2024-09-24 09:30:19 -0300

    refacture.wagner correção do campo name para nome

--- MVC-app/app/Models/User.php
+++ MVC-app/app/Models/User.php
@@ -6,8 +6,8 @@ class User{
     Public function getUsers(){
         //Em um caso real, você teria a conexão com o banco de dados
         return [
-            ['id'=>1,'name'=>'João', 'email'=>'joao@email.com'],
-            ['id' =>2,'name'=>'Maria', 'email'=>'maria@email.com']
+            ['id'=>1,'nome'=>'João', 'email'=>'joao@email.com'],
+            ['id' =>2,'nome'=>'Maria', 'email'=>'maria@email.com']
         ];
     }
 }
