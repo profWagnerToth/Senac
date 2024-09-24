9a2ceac1ec4a185acdb4f77557c9c7d1782b99fc
Prof Wagner Ramos Toth - Senac Registro <wagner.rtoth@sp.senac.br>
2024-09-24 08:32:36 -0300

    initial.wagner user.php

---
+++ MVC-app/app/Models/User.php
@@ -0,0 +1,14 @@
+<?php
+#Este arquivo será responsável por lidar com os dados dos usuários.
+
+class User{
+    //Simulando a obtenção de dados do usuário
+    Public function getUser(){
+        //Em um caso real, você teria a conexão com o banco de dados
+        return [
+            ['id'=>1,'name'=>'João', 'email'=>'joao@email.com'],
+            ['id' =>2,'name'=>'Maria', 'email'=>'maria@email.com']
+        ];
+    }
+}
+?>
