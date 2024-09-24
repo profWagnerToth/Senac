907639b833cd0a34607a8689fe53984a1b5bd67e
Prof Wagner Ramos Toth - Senac Registro <wagner.rtoth@sp.senac.br>
2024-09-24 09:19:27 -0300

    refacture.wagner acerto caminho app.php e controller.php

--- MVC-app/Public/index.php
+++ MVC-app/Public/index.php
@@ -2,8 +2,8 @@
 #Arquivo principal que inicia o aplicativo.Ele deve estar na pasta Public
 
 //Incluir os arquivos principais da aplicação
-require_once '../app/Core/App.php';
-require_once '../app/Core/Controller.php';
+require_once '../Public/Core/App.php';
+require_once '../Public/Core/Controller.php';
 
 //Inicia a aplicação
 $app = new App();
