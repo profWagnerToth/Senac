35e7fb56660237e2a7e8801ea9bd0d32f442daf0
Prof Wagner Ramos Toth - Senac Registro <wagner.rtoth@sp.senac.br>
2024-09-27 11:21:17 -0300

    initial.wagner professor.php

---
+++ Academia-app/models/Professor.php
@@ -0,0 +1,17 @@
+<?php
+include ('bd/conexao.php');
+
+class Professor{
+    private $db;
+
+    public function __construct(){
+        $this->db=Conexao::novaConexao();
+    }
+
+    public function cadastrarProfessor($nome,$email,$telefone, $especialidade){
+        $cadProf = $this->db->prepare("INSERT INTO professores (nome,email,telefone,especialidade) VALUES (?,?,?,?)");
+
+        $cadProf->execute([$nome,$email,$telefone,$especialidade]);
+    }
+}
+?>
