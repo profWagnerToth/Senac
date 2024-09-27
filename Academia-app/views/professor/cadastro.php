8512a49bac798e5a552b0248a983eb18bdafd98e
Prof Wagner Ramos Toth - Senac Registro <wagner.rtoth@sp.senac.br>
2024-09-27 12:00:01 -0300

    initial.wagner view Professor cadastro.php

---
+++ Academia-app/views/professor/cadastro.php
@@ -0,0 +1,29 @@
+<?php 
+include '../../includes/header.php';
+?>
+<div class="container mt-5">
+    <h2>Cadastro de Professor</h2>
+    <form action="../../controller/ProfessorController.php?action=cadastrar" method="POST">
+        <div class="mb-3">
+            <label for="nome" class="'form-label">Nome:</label>
+            <input type="text" class="'form-control" id="nome" name="nome" required>
+        </div>    
+        
+        <div class="mb-3">
+            <label for="email" class="'form-label">Email:</label>
+            <input type="email" class="'form-control" id="email" name="email" required>
+        </div>    
+        
+        <div class="mb-3">
+            <label for="telefone" class="'form-label">Telefone:</label>
+            <input type="text" class="'form-control" id="telefone" name="telefone" required>
+        </div>    
+
+        
+        <div class="mb-3">
+            <label for="especialidade" class="'form-label">Especialidade:</label>
+            <input type="text" class="'form-control" id="especialidade" name="especialidade" required>
+        </div>    
+    </form>
+    <?php include('../../includes/footer.php')?>
+</div>
