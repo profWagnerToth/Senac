9dceffb431420b7c9be547c1b60f9ce93907914f
profWagnerToth <wagnertoth@outlook.com>
2025-01-28 10:38:08 -0300

    ..

--- PhpXFirebase/index.php
+++ PhpXFirebase/index.php
@@ -5,8 +5,12 @@ require __DIR__.'/vendor/autoload.php';
 //Acessando a biblioteca Kreait Firebase PhP SDK
 use Kreait\Firebase\Factory;
 
+// Acessando a biblioteca ServiceAccount Firebase PHP SDK
+use Kreait\Firebase\ServiceAccount;
+
 //Criando conexão com BD Realtime Database Firebase
 $factory = (new Factory())
+//->withServiceAccount(__DIR__.'/firebase_chave.json') 
 ->withDatabaseUri('https://fir-php-e50ee-default-rtdb.firebaseio.com/'); //Link de acesso ao Realtime Databse do Firebase.
 
 //Instanciando o serviço do Realtime Database
